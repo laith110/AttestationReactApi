545c24ff3d6215bc2754aa7959df9899254b721c
vasilevaleksei <mrl19_97@mail.ru>
2023-04-19 15:53:58 +0300

    qwe

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { motion } from "framer-motion"
 import '../App.css' 
 
 const Description = () => {
@@ -7,7 +6,7 @@ const Description = () => {
     <div>
          
       <div>
-    <div>Description</div>
+    <div>Описание</div>
 
 
 
